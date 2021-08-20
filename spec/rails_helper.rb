69fd6c81fb9d62aa212e7cd824f5d24690bd9e52
Taylor Varoglu <tava8398@colorado.edu>
2021-08-19 18:24:22 -0600

    Add authentication and user controllers to lib and spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,10 +43,12 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
